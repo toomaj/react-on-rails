361452354612b116e1dd0f8ee7284a1a7bc66714
Toomaj Boloorian <toomaj@protonmail.com>
2020-07-24 21:37:57 +1000

    TB: Create Rails + React

---
+++ README.md
@@ -0,0 +1,13 @@
+### React + Rails & jRuby
+
+```
+rvm install 2.5.7
+rvm install jruby-9.2.12.0
+rvm --default use jruby-9.2.12.0
+gem install bundler
+bundle install
+bundle exec rails db:create db:migrate
+bundle exec rails s
+```
+
+[localhost:3000]()
---
+++ config/warble.rb
@@ -0,0 +1,17 @@
+# Warbler web application assembly configuration file
+Warbler::Config.new do |config|
+  config.features = %w(compiled)
+  config.dirs = %W(app config lib vendor db)
+  config.includes = %w(Rakefile config.ru jetty-web.xml)
+  config.excludes = FileList['lib/tasks', 'lib/spec', 'config/web.xml.erb', 'db/seeds.rb']
+  config.bundler = true
+  config.bundle_without = %w(development test ci deployment assets)
+  config.gem_dependencies = true
+  config.gem_excludes = [/^(test|spec)\//]
+  config.jar_name = 'react-on-rails'
+  config.webxml.rails.env = ENV['RAILS_ENV'] || 'test'
+  config.webxml.jruby.compat.version = "1.9"
+  config.webxml.jruby.min.runtimes = 1
+  config.webxml.jruby.max.runtimes = 1
+  config.webserver = 'jetty'
+end
