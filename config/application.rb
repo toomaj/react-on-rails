4023384638e8929791d0eb37ce016ac4754cbe3b
Toomaj Boloorian <toomaj@protonmail.com>
2020-07-25 20:11:54 +1000

    Update gems, add home & jetty-web.xml

--- config/application.rb
+++ config/application.rb
@@ -15,8 +15,8 @@ module ReactOnRails
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    config.generators do |g|
-      g.template_engine :slim
-    end
+    # config.generators do |g|
+      # g.template_engine :slim
+    # end
   end
 end
--- config/warble.rb
+++ config/warble.rb
@@ -1,9 +1,8 @@
 # Warbler web application assembly configuration file
 Warbler::Config.new do |config|
-  config.features = %w(compiled)
+  config.features = %w(compiled executable)
   config.dirs = %W(app config lib vendor db)
-  # config.includes = %w(Rakefile config.ru jetty-web.xml)
-  config.includes = %w(Rakefile config.ru)
+  config.includes = %w(Rakefile config.ru jetty-web.xml)
   config.excludes = FileList['lib/tasks', 'lib/spec', 'config/web.xml.erb', 'db/seeds.rb']
   config.bundler = true
   config.bundle_without = %w(development test ci deployment assets)
@@ -11,8 +10,9 @@ Warbler::Config.new do |config|
   config.gem_excludes = [/^(test|spec)\//]
   config.jar_name = 'react-on-rails'
   config.webxml.rails.env = ENV['RAILS_ENV'] || 'test'
-  config.webxml.jruby.compat.version = "1.9"
+
+  config.webserver = 'jetty'
+
   config.webxml.jruby.min.runtimes = 1
   config.webxml.jruby.max.runtimes = 1
-  config.webserver = 'jetty'
 end
